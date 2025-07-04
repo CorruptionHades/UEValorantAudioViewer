17c5519c02d8804c42109c7c2dc581503ed29547
CorruptionHades <corruptionhades@gmail.com>
2025-07-05 01:00:22 +0200

    msising file

---
+++ UEValorantAudioViewer/PathSelector.xaml.cs
@@ -0,0 +1,33 @@
+using System.Windows;
+using System.Windows.Controls;
+using UEValorantAudioViewer.utils;
+
+namespace UEValorantAudioViewer;
+
+public partial class PathSelector : UserControl
+{
+    private MainWindow _main;
+    public PathSelector(MainWindow main)
+    {
+        InitializeComponent();
+        _main = main;
+        PathBox.Text = Settings.settings.PaksFolder;
+        AesBox.Text = Settings.settings.AesKey;
+        OutFolder.Text = Settings.settings.OutputFolder;
+    }
+
+    private void Browse_Click(object sender, RoutedEventArgs e)
+    {
+        // Stub: File dialog
+        PathBox.Text = "H:\\Programme\\Valorant\\Riot Games\\VALORANT\\live\\ShooterGame\\Content\\Paks";
+    }
+
+    private void Confirm_Click(object sender, RoutedEventArgs e)
+    {
+        Settings.settings.PaksFolder = PathBox.Text;
+        Settings.settings.AesKey = AesBox.Text;
+        Settings.settings.OutputFolder = OutFolder.Text;
+        Settings.SaveSettings();
+        _main.ShowLoadingScreen();
+    }
+}
